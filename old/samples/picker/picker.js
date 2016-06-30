a27e32e20585d2d84ea0235e60365a3e06eec68c
Dmitriy Kaminskiy <dmitriy.kaminskiy@matrix42.com>
2016-06-30 12:24:36 +0300

    up

--- old/samples/picker/picker.js
+++ old/samples/picker/picker.js
@@ -1,104 +1,100 @@
-(function () {
-	'use strict';
+angular
+	.module('app')
+	.controller('PickerController', PickerController);
 
-	angular.module('app').controller('PickerController', PickerController);
+function PickerController($q) {
+	var vm         = this;
+	vm.readOnly    = true;
+	vm.isDisabled  = true;
+	
+	vm.pickerValue = 2;
 
-	PickerController.$inject = ['$q'];
+	vm.pickerData = [{id: 1, title: 'item 1'}, {id: 2, title: 'item 2'}, {id: 3, title: 'item 3'}, {
+		id: 4,
+		title: 'item 4'
+	}, {id: 104, title: 'filtered item 1'}, {id: 105, title: 'filtered item 2'}];
 
-	function PickerController($q) {
-		var vm         = this;
-		vm.readOnly    = true;
-		vm.isDisabled  = true;
-		
-		vm.pickerValue = 2;
+	vm.pickerArray = ['Item1', 'Item2', 'Item3', 'Item4', 'Item5', 'Item6'];
 
-		vm.pickerData = [{id: 1, title: 'item 1'}, {id: 2, title: 'item 2'}, {id: 3, title: 'item 3'}, {
-			id: 4,
-			title: 'item 4'
-		}, {id: 104, title: 'filtered item 1'}, {id: 105, title: 'filtered item 2'}];
+	vm.pickerArrayValue = 'Item5';
 
-		vm.pickerArray = ['Item1', 'Item2', 'Item3', 'Item4', 'Item5', 'Item6'];
+	var items = [
+		{
+			Value: 1,
+			DisplayString: 'ABA',
+			Description: 'Details for ABA'
+		},
+		{
+			Value: 2,
+			DisplayString: 'ABC',
+			Description: 'Details for ABC'
+		},
+		{
+			Value: 8,
+			DisplayString: 'Absolutely long Display String',
+			Description: 'Details for Absolutely long Display String'
+		},
+		{
+			Value: 3,
+			DisplayString: 'BBC',
+			Description: 'Details for BBC'
+		},
+		{
+			Value: 4,
+			DisplayString: 'BAC',
+			Description: 'Details for BAC'
+		},
+		{
+			Value: 5,
+			DisplayString: 'BCS',
+			Description: 'Details for BCS'
+		},
+		{
+			Value: 6,
+			DisplayString: 'AAA',
+			Description: 'Details for AAA'
+		},
+		{
+			Value: 7,
+			DisplayString: 'ABZ',
+			Description: 'Details for ABZ'
+		}
+	];
+	
+	vm.pickerLoadOnTypingValue = 5;
 
-		vm.pickerArrayValue = 'Item5';
+	vm.loadItems = function (searchText, ctrl, itms) {
 
-		var items = [
-			{
-				Value: 1,
-				DisplayString: 'ABA',
-				Description: 'Details for ABA'
-			},
-			{
-				Value: 2,
-				DisplayString: 'ABC',
-				Description: 'Details for ABC'
-			},
-			{
-				Value: 8,
-				DisplayString: 'Absolutely long Display String',
-				Description: 'Details for Absolutely long Display String'
-			},
-			{
-				Value: 3,
-				DisplayString: 'BBC',
-				Description: 'Details for BBC'
-			},
-			{
-				Value: 4,
-				DisplayString: 'BAC',
-				Description: 'Details for BAC'
-			},
-			{
-				Value: 5,
-				DisplayString: 'BCS',
-				Description: 'Details for BCS'
-			},
-			{
-				Value: 6,
-				DisplayString: 'AAA',
-				Description: 'Details for AAA'
-			},
-			{
-				Value: 7,
-				DisplayString: 'ABZ',
-				Description: 'Details for ABZ'
-			}
-		];
-		vm.pickerLoadOnTypingValue = 5;
-
-		vm.loadItems = function (searchText, ctrl, itms) {
-
-			if (itms) {
-				itms.forEach(function (itm) {
-					items.forEach(function (item) {
-						if (item.Value === itm.Value) {
-							itm.DisplayString = item.DisplayString;
-						}
-					});
-				});
-			} else {
-				console.log(searchText);
-				var res = [];
-
+		if (itms) {
+			itms.forEach(function (itm) {
 				items.forEach(function (item) {
-					if (item.DisplayString.toLowerCase().startsWith(searchText)) {
-						res.push(item);
+					if (item.Value === itm.Value) {
+						itm.DisplayString = item.DisplayString;
 					}
 				});
-				var retr = res.slice(0, 3);
-				return $q.when({items: retr, searchText: searchText, all: res.length !== retr.length});
-			}
-		};
+			});
+		} else {
+			var res = [];
 
-		vm.navigateSelectedItem = function (item) {
-			alert(JSON.stringify(item));
-		};
+			items.forEach(function (item) {
+				if (item.DisplayString.toLowerCase().startsWith(searchText)) {
+					res.push(item);
+				}
+			});
+			var retr = res.slice(0, 3);
+			return $q.when({items: retr, searchText: searchText, all: res.length !== retr.length});
+		}
+	};
 
-		vm.browseLookup = function () {
-			if (confirm('Add items?')) {
-				return $q.when(vm.pickerData[2]);
-			} else {
-				return $q.when();
-			}
-		};
-	}
-})();
+	vm.navigateSelectedItem = function (item) {
+		alert(JSON.stringify(item));
+	};
+
+	vm.browseLookup = function () {
+		if (confirm('Add items?')) {
+			return $q.when(vm.pickerData[2]);
+		} else {
+			return $q.when();
+		}
+	};
+}
--- old/samples/pickerTest/pickerTest.js
+++ old/samples/pickerTest/pickerTest.js
@@ -1,107 +1,103 @@
-(function () {
-	'use strict';
-	angular.module('app').controller('PickerTestController', PickerTestController);
+angular
+	.module('app')
+	.controller('PickerTestController', PickerTestController);
 
-	PickerTestController.$inject = ['$q', '$timeout', 'mx.internationalization', '$scope'];
+function PickerTestController($q, $timeout, internationalization, $scope) {
+	var vm          = this;
+	vm.readOnly     = true;
+	vm.isDisabled   = true;
+	vm.valueHistory = [];
+	var pickerData  = [];
 
-	function PickerTestController($q, $timeout, internationalization, $scope) {
-		var vm          = this;
-		vm.readOnly     = true;
-		vm.isDisabled   = true;
-		vm.valueHistory = [];
-		var pickerData  = [];
-
-		for (var i = 1; i < 1000; i++) {
-			pickerData.push({id: i, title: 'C@#$%^&*_+~}":?<MЁ!"№;%:?*()_+/ЪЭ,_ä' + i});
-		}
-
-		vm.pickerData        = pickerData;
-		vm.pickerValue       = 2;
-		vm.pickerArray       = ['Item1', 'Item2', 'Item3', 'Item4', 'Item5', 'Item6'];
-		vm.pickerArrayValue  = 'Item5';
-		vm.pickerArray2      = ['Item1', 'Item2', 'Item3', 'Item4', 'Item5', 'Item6'];
-		vm.pickerArrayValue2 = 'Item3';
-
-		vm.selectedLanguage = internationalization.language;
-
-		if (vm.selectedLanguage) {
-			internationalization.language = vm.selectedLanguage;
-		}
-		vm.languages = ['en', 'de'];
-
-		$scope.$watch('vm.selectedLanguage', function (newVal) {
-			if (newVal === internationalization.language) {
-				return;
-			}
-
-			vm.refresh                    = true;
-			internationalization.language = vm.selectedLanguage;
-
-			$timeout(function () {
-				vm.refresh = false;
-			});
-		});
-
-		vm.localization = {
-			'userInput.required': 'User name must be populated with some data',
-			'custValidation.error': 'change user to 123'
-		};
-
-		var items = [
-			{
-				Value: 1,
-				DisplayString: 'ABA'
-			},
-			{
-				Value: 2,
-				DisplayString: 'ABC'
-			},
-			{
-				Value: 3,
-				DisplayString: 'BBC'
-			},
-			{
-				Value: 4,
-				DisplayString: 'BAC'
-			},
-			{
-				Value: 5,
-				DisplayString: 'BCS'
-			},
-			{
-				Value: 6,
-				DisplayString: 'AAA'
-			}
-		];
-
-		vm.pickerItems = items;
-		vm.pickerSelectedItem = 1;
-		//vm.pickerLoadOnTypingValue = items[0];
-
-		vm.navigateSelectedItem = function (item) {
-			var newItem = item;
-			newItem.$$hashKey = 'hashKey';
-			alert(JSON.stringify(newItem));
-		};
-
-		vm.loadItems = function (searchText) {
-
-			console.log(searchText);
-			if (!vm.entityClassName) {
-				return $q.when([]);
-			}
-			var res = [];
-			items.forEach(function (item) {
-				if (item.DisplayString.toLowerCase().startsWith(searchText)) {
-					res.push(item);
-				}
-			});
-
-			return $q.when({items: res, searchText: searchText});
-
-
-			//return mxEntityService.getFragments(vm.entityClassName, vm.filterExpression, searchText);
-		};
+	for (var i = 1; i < 1000; i++) {
+		pickerData.push({id: i, title: 'C@#$%^&*_+~}":?<MЁ!"№;%:?*()_+/ЪЭ,_ä' + i});
 	}
 
-})();
+	vm.pickerData        = pickerData;
+	vm.pickerValue       = 2;
+
+	vm.pickerArray       = ['Item1', 'Item2', 'Item3', 'Item4', 'Item5', 'Item6'];
+	vm.pickerArrayValue  = 'Item5';
+
+	vm.pickerArray2      = ['Item1', 'Item2', 'Item3', 'Item4', 'Item5', 'Item6'];
+	vm.pickerArrayValue2 = 'Item3';
+
+	vm.selectedLanguage = internationalization.language;
+
+	if (vm.selectedLanguage) {
+		internationalization.language = vm.selectedLanguage;
+	}
+	vm.languages = ['en', 'de'];
+
+	$scope.$watch('vm.selectedLanguage', function (newVal) {
+		if (newVal === internationalization.language) {
+			return;
+		}
+
+		vm.refresh                    = true;
+		internationalization.language = vm.selectedLanguage;
+
+		$timeout(function () {
+			vm.refresh = false;
+		});
+	});
+
+	vm.localization = {
+		'userInput.required': 'User name must be populated with some data',
+		'custValidation.error': 'change user to 123'
+	};
+
+	var items = [
+		{
+			Value: 1,
+			DisplayString: 'ABA'
+		},
+		{
+			Value: 2,
+			DisplayString: 'ABC'
+		},
+		{
+			Value: 3,
+			DisplayString: 'BBC'
+		},
+		{
+			Value: 4,
+			DisplayString: 'BAC'
+		},
+		{
+			Value: 5,
+			DisplayString: 'BCS'
+		},
+		{
+			Value: 6,
+			DisplayString: 'AAA'
+		}
+	];
+
+	vm.pickerItems = items;
+	vm.pickerSelectedItem = 1;
+
+	vm.navigateSelectedItem = function (item) {
+		var newItem = item;
+		newItem.$$hashKey = 'hashKey';
+		alert(JSON.stringify(newItem));
+	};
+
+	vm.loadItems = function (searchText) {
+		
+		if (!vm.entityClassName) {
+			return $q.when([]);
+		}
+
+		var res = [];
+
+		items.forEach(function (item) {
+			if (item.DisplayString.toLowerCase().startsWith(searchText)) {
+				res.push(item);
+			}
+		});
+
+		return $q.when({items: res, searchText: searchText});
+	};
+
+}
